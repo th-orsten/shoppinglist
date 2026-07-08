3005a5a65feb08d437b4a327c3b20fc182be40ab
Thorsten <thorsten-schoenrock@gmx.de>
2026-07-08 16:17:45 +0200

    error login.js

--- netlify/functions/login.js
+++ netlify/functions/login.js
@@ -63,14 +63,15 @@ exports.handler = async (event) => {
       }),
     };
   } catch (err) {
-  console.error(err);
+    console.error(err);
 
-  return {
-    statusCode: 500,
-    body: JSON.stringify({
-      success: false,
-      error: err.message,
-      stack: err.stack
-    })
-  };
-}
+    return {
+      statusCode: 500,
+      body: JSON.stringify({
+        success: false,
+        error: err.message,
+        stack: err.stack
+      })
+    };
+  }
+};
